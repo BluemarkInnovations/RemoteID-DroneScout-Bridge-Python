e1e988aceb46ea44f1cfcedffa8188ac5bf7528f
Roel Schiphorst <roel@bluemark.io>
2025-03-01 14:59:42 +0100

    decode basic ID/UA type and byte decoding errors

--- modules/odid.py
+++ modules/odid.py
@@ -23,6 +23,58 @@ def clean_string(string):
 
     return string
 
+def decode_basicID_ID_type(IDType):
+    string = ""
+    if IDType == 0:
+        string = "None"
+    elif IDType == 1:
+        string = "Serial Number"
+    elif IDType == 2:
+        string = "CAA Registration ID"
+    elif IDType == 3:
+        string = "UTM Assigned UUID"
+    elif IDType == 4:
+        string = "specific session ID"
+
+    return string
+
+def decode_basicID_UA_type(UAType):
+    string = ""
+    if UAType == 0:
+        string = "None"
+    elif UAType == 1:
+        string = "Aeroplane"
+    elif UAType == 2:
+        string = "Helicopter (or Multirotor)"
+    elif UAType == 3:
+        string = "Gyroplane"
+    elif UAType == 4:
+        string = "Hybrid Lift"
+    elif UAType == 5:
+        string = "Ornithopter"
+    elif UAType == 6:
+        string = "Glider"
+    elif UAType == 7:
+        string = "Kite"
+    elif UAType == 8:
+        string = "Free Balloon"
+    elif UAType == 9:
+        string = "Captive Balloon"
+    elif UAType == 10:
+        string = "Airship (such as a blimp)"
+    elif UAType == 11:
+        string = "Free Fall/Parachute (unpowered)"
+    elif UAType == 12:
+        string = "Rocket"
+    elif UAType == 13:
+        string = "Tethered Powered Aircraft"
+    elif UAType == 14:
+        string = "Ground Obstacle"
+    elif UAType == 15:
+        string = "Other"
+
+    return string
+
 def decode_location_status(status):
     string = ""
     if status == 0:
@@ -92,12 +144,12 @@ def print_message_pack(payload, size):
 			print("ProtoVersion: %i" % ProtoVersion)
 
 def print_basicID(payload):
-	UAType = payload[1] >> 4
-	IDType = payload[1] & 0x0F
+	IDType = payload[1] >> 4
+	UAType = payload[1] & 0x0F
 	UASID = payload[2:2 + ODID_ID_SIZE]
 
-	print("UAType: %i" % UAType)
-	print("IDType: %i" % IDType)
+	print("UAType: %s" % decode_basicID_UA_type(UAType))
+	print("IDType: %s" % decode_basicID_ID_type(IDType))
 	print("UASID: %s" % clean_string(bytes(UASID).decode('ascii')))
 
 def print_location(payload):
@@ -121,19 +173,19 @@ def print_location(payload):
 	TSAccuracy = payload[23] & 0x0F
 
 	print("Status: %s" % decode_location_status(Status))
-	print("SpeedMult: %i" % SpeedMult)
-	print("EWDirection: %i" % EWDirection)
-	print("HeightType: %s" % decode_location_height_type(HeightType))
+	print("Speed Mult: %i" % SpeedMult)
+	print("EW Direction: %i" % EWDirection)
+	print("Height Type: %s" % decode_location_height_type(HeightType))
 	print("Direction: %i" % Direction)
-	print("SpeedHorizontal: %i" % SpeedHorizontal)
-	print("SpeedVertical: %i" % SpeedVertical)
+	print("Speed Horizontal: %i" % SpeedHorizontal)
+	print("Speed Vertical: %i" % SpeedVertical)
 	print("Latitude: %f" % (float(Latitude)/(10*1000*1000)))
 	print("Longitude: %f" % (float(Longitude)/(10*1000*1000)))
-	print("AltitudeBaro: %f" % ((int(AltitudeBaro[0]) - int(2000))/2))
-	print("AltitudeGeo: %f" % ((int(AltitudeGeo[0]) - int(2000))/2))
+	print("Altitude Baro: %f" % ((int(AltitudeBaro[0]) - int(2000))/2))
+	print("Altitude Geo: %f" % ((int(AltitudeGeo[0]) - int(2000))/2))
 	print("Height: %f" % ((int(Height[0]) - int(2000))/2))
-	print("HorizAccuracy: %i" % int(HorizAccuracy))
-	print("VertAccuracy: %i" % int(VertAccuracy))
-	print("SpeedAccuracy: %i" % int(SpeedAccuracy))
+	print("Horiz Accuracy: %i" % int(HorizAccuracy))
+	print("Vert Accuracy: %i" % int(VertAccuracy))
+	print("Speed Accuracy: %i" % int(SpeedAccuracy))
 	print("Timestamp: %s" % decode_location_timestamp(TimeStamp))
 	print("Timestamp Accuracy: %i" % int(TSAccuracy))
